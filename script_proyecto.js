79897b846aa7a3b65ed7ee544dff711fede9f807
rafi412 <127504757+rafi412@users.noreply.github.com>
2023-04-10 23:05:35 +0100

    Add files via upload

---
+++ script_proyecto.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+   alert("¡Bienvenido a Deportes Guerra!");
+}
+
+window.onload = function() {
+  var boton1 = document.querySelector('#boton_1');
+  boton1.addEventListener('click', function() {
+    alert('Balón oficial de la NBA en esta temporada.');
+  });
+
+  var boton2 = document.querySelector('#boton_2');
+  boton2.addEventListener('click', function() {
+    alert('Zapatillas nike para moverte mejor que nunca en el campo.');
+  });
+}
